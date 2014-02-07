369dd2c534db35f4722a2f5811093e6c81624c32
Brandon24 <brandonreinert24@gmail.com>
2014-02-07 10:08:52 -0500

    Chapter1problems

---
+++ Name/Address/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Brandon Craig Reinert");
+            System.Console.WriteLine("2705 Carolyn Street");
+            System.Console.WriteLine("Easton,Pennsylvania");
+        }
+    }
+}
---
+++ Name/Tree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B");
+            Console.WriteLine("BBB");
+            Console.WriteLine("BBBBB");
+            Console.WriteLine("BBBBBB");
+            Console.WriteLine("B");
+        }
+    }
+}
